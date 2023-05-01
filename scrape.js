72b31bf8d9eae5c03551161bc255392b2d57c63d
Roomail Nadeem <130645639+RoomailNadeem@users.noreply.github.com>
2023-05-02 03:34:17 +0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,17 +1,20 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    executablePath: '/usr/bin/google-chrome-stable', // or wherever chrome is installed on your machine
+    args: ['--no-sandbox', '--disable-setuid-sandbox'] // may be necessary depending on your system
+  });
   const page = await browser.newPage();
   await page.goto('https://example.com');
   const imageUrls = await page.evaluate(() => {
     const images = document.querySelectorAll('img');
     return Array.from(images).map(img => img.src);
   });
-  await Promise.all(imageUrls.map(url => {
+  await Promise.all(imageUrls.map(async url => {
     const fileName = url.split('/').pop();
-    return page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './images'});
-    return page.goto(url, {waitUntil: 'networkidle2'});
+    await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './images'});
+    await page.goto(url, {waitUntil: 'networkidle2'});
   }));
   await browser.close();
 })();
