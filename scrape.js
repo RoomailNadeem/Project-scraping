dcdec30c7013b289b611b25d99f9eb92ae9a4dcb
Roomail Nadeem <130645639+RoomailNadeem@users.noreply.github.com>
2023-05-02 15:02:41 +0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -2,19 +2,13 @@ const puppeteer = require('puppeteer-core');
 
 (async () => {
   const browser = await puppeteer.launch({
-    args: chromeArgs,
-    executablePath: 'C:\Program Files\Google\Chrome',
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ],
+    executablePath: 'C:\\Program Files\\Google\\Chrome\\chrome.exe',
+    headless: true
   });
-
-  const page = await browser.newPage();
-
-  // rest of your code goes here
-
-})();
-
-
 
   const page = await browser.newPage();
   await page.goto('https://sol-testing.webflow.io/');
@@ -22,10 +16,12 @@ const puppeteer = require('puppeteer-core');
     const images = document.querySelectorAll('img');
     return Array.from(images).map(img => img.src);
   });
+
   await Promise.all(imageUrls.map(async url => {
     const fileName = url.split('/').pop();
     await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './images'});
     await page.goto(url, {waitUntil: 'networkidle2'});
   }));
+
   await browser.close();
 })();
